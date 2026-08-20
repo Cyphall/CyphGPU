9c898bcbbdb11c7bd2e2c12e777786cfacda6538
Cyphall <cyphallbraus@gmail.com>
2026-08-20 19:35:23 +0200

    Don't rely on exception when shader entry is not found

--- src/cpp/CyphGPU/ShaderBundle.cpp
+++ src/cpp/CyphGPU/ShaderBundle.cpp
@@ -9,16 +9,16 @@ cgpu::ShaderBundle::ShaderBundle(cmrc::embedded_filesystem filesystem):
 
 std::optional<std::span<const std::byte>> cgpu::ShaderBundle::tryGetShaderBlob(std::string_view identifier) const
 {
-	try
-	{
-		cmrc::file spirv_file = m_filesystem.open(std::format("{}.spv", identifier));
-		return std::span{
-			reinterpret_cast<const std::byte*>(spirv_file.begin()),
-			spirv_file.size(),
-		};
-	}
-	catch (...)
+	std::string path = std::format("{}.spv", identifier);
+
+	if (!m_filesystem.is_file(path))
 	{
 		return std::nullopt;
 	}
+
+	cmrc::file spirv_file = m_filesystem.open(path);
+	return std::span{
+		reinterpret_cast<const std::byte*>(spirv_file.begin()),
+		spirv_file.size(),
+	};
 }
