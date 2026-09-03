dccc2eb9f56efff0d9ba38ab115fcc325ae1cfc6
Cyphall <cyphallbraus@gmail.com>
2026-09-03 19:48:12 +0200

    Removed unused C flags

--- cmake/toolchains/windows-clang-cl.cmake
+++ cmake/toolchains/windows-clang-cl.cmake
@@ -2,5 +2,4 @@ set(CMAKE_C_COMPILER clang-cl.exe)
 set(CMAKE_CXX_COMPILER clang-cl.exe)
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-set(CMAKE_C_FLAGS_INIT "/Zc:__cplusplus /utf-8 /arch:AVX2")
 set(CMAKE_CXX_FLAGS_INIT "/Zc:__cplusplus /utf-8 /arch:AVX2")
